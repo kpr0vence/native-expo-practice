6f49b4e49c257ac18f964247c58e48b382b5d21c
Kati Provence <kproven1@unca.edu>
2025-09-23 21:56:01 -0400

    Followed use an image picker section of the expo tutorial

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,36 @@
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 const placeholderImage = require('@/assets/images/withglasses.png')
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const picker = async() => {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ['images'],
+        allowsEditing: true,
+        quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      alert("No image selected.")
+    }
+  }
+
+
+
+
   return (
     <View style={styles.container}>
-      <ImageViewer imgSource={placeholderImage}/>
+      <ImageViewer imgSource={placeholderImage} selectedImage={selectedImage}/>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme={'primary'}/>
-        <Button label="Use this photo" />
+        <Button label="Choose a photo" theme={'primary'} onPress={picker}/>
+        <Button label="Use this photo"/>
       </View>
     </View>
   );
